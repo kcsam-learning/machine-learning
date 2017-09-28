eebce8e46aa3bc2a7f0104271df1a241b157dd5a
samkahchiin <samkahchiin@gmail.com>
2017-09-28 22:39:01 +0800

    [Finishes] Done normal equation

--- coursera/ex1/normalEqn.m
+++ coursera/ex1/normalEqn.m
@@ -1,6 +1,6 @@
 function [theta] = normalEqn(X, y)
-%NORMALEQN Computes the closed-form solution to linear regression 
-%   NORMALEQN(X,y) computes the closed-form solution to linear 
+%NORMALEQN Computes the closed-form solution to linear regression
+%   NORMALEQN(X,y) computes the closed-form solution to linear
 %   regression using the normal equations.
 
 theta = zeros(size(X, 2), 1);
@@ -9,15 +9,8 @@ theta = zeros(size(X, 2), 1);
 % Instructions: Complete the code to compute the closed form solution
 %               to linear regression and put the result in theta.
 %
-
-% ---------------------- Sample Solution ----------------------
-
-
-
-
-% -------------------------------------------------------------
-
-
-% ============================================================
-
+% y = X * theta;
+% NOTE: We can't do division in matrix
+% y * X' = X * X' * theta;
+theta = pinv(X' * X) * (X' * y)
 end
