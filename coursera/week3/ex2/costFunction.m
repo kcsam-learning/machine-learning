2f9795ed71240fdd52b00f47d2f3a7f0eb26c767
samkahchiin <samkahchiin@gmail.com>
2017-10-02 23:39:42 +0800

    [Finishes] Done costFunction.m

--- coursera/week3/ex2/costFunction.m
+++ coursera/week3/ex2/costFunction.m
@@ -3,6 +3,7 @@ function [J, grad] = costFunction(theta, X, y)
 %   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
 %   parameter for logistic regression and the gradient of the cost
 %   w.r.t. to the parameters.
+%   w.r.t = with respect to
 
 % Initialize some useful values
 m = length(y); % number of training examples
@@ -11,22 +12,15 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 %
 % Note: grad should have the same dimensions as theta
-%
+prediction = sigmoid(X * theta);
+total = -y' * log(prediction) - (1 - y)' * log(1 .- prediction)
+J = total / m
 
-
-
-
-
-
-
-
-% =============================================================
-
+grad = (X' * (prediction - y)) / m
 end
